8b4338a691d7017b9abd01e3a7835f2bbf08efb1
sanjeeshbs <sanjeeshbs@gmail.com>
2023-10-09 14:41:27 +0000

    test commit

--- ecommerce/ecommerce/page/products_page/products_page.js
+++ ecommerce/ecommerce/page/products_page/products_page.js
@@ -1,7 +1,7 @@
 frappe.pages['products-page'].on_page_load = function(wrapper) {
 	var page = frappe.ui.make_app_page({
 		parent: wrapper,
-		title: 'products',
+		title: 'products page',
 		single_column: true
 	});
 }
